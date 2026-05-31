0fc2ebbc96c7e2ae4f58792d572ba9de8032408d
MarioJeconiah <mariopurba40@gmail.com>
2026-05-31 17:02:30 +0700

    deployy

--- smartLaundry/src/main/java/com/laundry/service/ReportService.java
+++
@@ -1,7 +0,0 @@
-package com.laundry.service;
-
-import com.laundry.dto.response.ReportResponse;
-
-public interface ReportService {
-    ReportResponse getReport();
-}
--- smartLaundry/src/main/java/com/laundry/service/TransactionService.java
+++
@@ -1,9 +0,0 @@
-package com.laundry.service;
-
-import com.laundry.dto.request.TransactionRequest;
-import com.laundry.dto.response.TransactionResponse;
-
-public interface TransactionService {
-    TransactionResponse getTransactionById(Long id);
-    TransactionResponse createTransaction(TransactionRequest request);
-}
